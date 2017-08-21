d25a37f875b1371ab741dc26d1da134d97f18632
Shlok Natarajan <shloknatarajan@users.noreply.github.com>
2017-08-20 21:19:39 -0500

    finished first 3 assignments

---
+++ length.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int length;
+    double inches, yards, feet;
+    cout << "Enter a length (cm)" << endl;
+    cin >> length;
+    inches = length / 2.54;
+    yards = inches / 36;
+    yards = int(yards);
+    inches = inches - yards * 36;
+    feet = inches / 12;
+    feet = int(feet);
+    inches = inches - feet * 12;
+    inches = int(inches);
+    cout << yards << " yard(s), " << feet << " feet, " << inches << " inch(es)" << endl;
+}
---
+++ picture.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name;
+    cout << 
+    "  /\\___/\\   \n  |0 + 0|   \n  \\__-__/   \n     |      \n   / | \\    \n  /  |  \\   \n     |      \n     |~~~~~\?\n    / \\     \n   /   \\    \n" << endl <<
+    "Please enter a one word name for my picture: ";
+    cin >> name;
+    cout << "I like it! I will call it \'" << name << "\'" << endl;
+
+}
