f1e30bc0687134c396346fb4a928a21446f1bebc
Shlok Natarajan <shloknatarajan@users.noreply.github.com>
2017-08-29 10:40:12 -0500

    Finished assignment 2 paycheck

--- paycheck.cpp
+++ paycheck.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main() {
     string name;
     float gross, fed, state, ss, med, pension, health, net;
-    cout << "Employee name: " << endl;
+    cout << "Employee name: ";
     getline(cin, name);
-    cout << "\nGross Pay: " << endl;
+    cout << "Gross Pay: ";
     cin >> gross;
 
+    // Calculations of taces
     fed = gross * 0.15;
     state = gross * 0.035;
     ss = gross * 0.0575;
@@ -17,12 +18,26 @@ int main() {
     pension = gross * 0.05;
     health = 75;
     net = gross - fed - state - ss - med - pension - health;
-    cout << name << endl;
-    cout.width(35);
-    cout << setprecision(2) << fixed;
-    cout << left << net << endl;
-    cout << left << "Gross Amount" << right << gross;
 
+    //Printing Data
+    cout << "\n" << name << endl;
+    cout << setw(18) << left << setfill('.') <<  "Gross Amount: ";
+    cout << setw(18) << setprecision(2) << fixed << right << setfill('.') << gross << endl;
+    cout << setw(18) << left << setfill('.') <<  "Federal Tax: ";
+    cout << setw(18) << right << setfill('.') << fed << endl;
+    cout << setw(18) << left << setfill('.') <<  "State Tax: ";
+    cout << setw(18) << right << setfill('.') << state << endl;
+    cout << setw(25) << left << setfill('.') <<  "Social Security Tax: ";
+    cout << setw(11) << right << setfill('.') << ss << endl;
+    cout << setw(25) << left << setfill('.') <<  "Medicare/Medicaid Tax: ";
+    cout << setw(11) << right << setfill('.') << med << endl;
+    cout << setw(18) << left << setfill('.') <<  "Pension Plan: ";
+    cout << setw(18) << right << setfill('.') << pension << endl;
+    cout << setw(18) << left << setfill('.') <<  "Health Insurance: ";
+    cout << setw(18) << right << setfill('.') << health << endl;
+    cout << setw(18) << left << setfill('.') <<  "Net Pay: ";
+    cout << setw(18) << right << setfill('.') << net << endl;
+    
     /*cout.width(45);
     cout << setfill('*') << setw(45);
     cout << left << "hi" << right << "Hola Amigos" << endl;*/
