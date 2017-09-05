6c988688190572538035ab6afe3b96951d28b714
Shlok Natarajan <shloknatarajan@users.noreply.github.com>
2017-09-05 10:20:40 -0500

    fixed paycheck

--- paycheck.cpp
+++ paycheck.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 int main() {
     string name;
     float gross, fed, state, ss, med, pension, health, net;
+    ofstream fout;
     cout << "Employee name: ";
     getline(cin, name);
     cout << "Gross Pay: ";
     cin >> gross;
+    fout.open("pay.txt");
 
     // Calculations of taces
     fed = gross * 0.15;
@@ -20,23 +23,23 @@ int main() {
     net = gross - fed - state - ss - med - pension - health;
 
     //Printing Data
-    cout << "\n" << name << endl;
-    cout << setw(18) << left << setfill('.') <<  "Gross Amount: ";
-    cout << setw(18) << setprecision(2) << fixed << right << setfill('.') << gross << endl;
-    cout << setw(18) << left << setfill('.') <<  "Federal Tax: ";
-    cout << setw(18) << right << setfill('.') << fed << endl;
-    cout << setw(18) << left << setfill('.') <<  "State Tax: ";
-    cout << setw(18) << right << setfill('.') << state << endl;
-    cout << setw(25) << left << setfill('.') <<  "Social Security Tax: ";
-    cout << setw(11) << right << setfill('.') << ss << endl;
-    cout << setw(25) << left << setfill('.') <<  "Medicare/Medicaid Tax: ";
-    cout << setw(11) << right << setfill('.') << med << endl;
-    cout << setw(18) << left << setfill('.') <<  "Pension Plan: ";
-    cout << setw(18) << right << setfill('.') << pension << endl;
-    cout << setw(18) << left << setfill('.') <<  "Health Insurance: ";
-    cout << setw(18) << right << setfill('.') << health << endl;
-    cout << setw(18) << left << setfill('.') <<  "Net Pay: ";
-    cout << setw(18) << right << setfill('.') << net << endl;
+    fout << "\n" << name << endl;
+    fout << setw(18) << left << setfill('.') <<  "Gross Amount: ";
+    fout << setw(18) << setprecision(2) << fixed << right << setfill('.') << gross << endl;
+    fout << setw(18) << left << setfill('.') <<  "Federal Tax: ";
+    fout << setw(18) << right << setfill('.') << fed << endl;
+    fout << setw(18) << left << setfill('.') <<  "State Tax: ";
+    fout << setw(18) << right << setfill('.') << state << endl;
+    fout << setw(25) << left << setfill('.') <<  "Social Security Tax: ";
+    fout << setw(11) << right << setfill('.') << ss << endl;
+    fout << setw(25) << left << setfill('.') <<  "Medicare/Medicaid Tax: ";
+    fout << setw(11) << right << setfill('.') << med << endl;
+    fout << setw(18) << left << setfill('.') <<  "Pension Plan: ";
+    fout << setw(18) << right << setfill('.') << pension << endl;
+    fout << setw(18) << left << setfill('.') <<  "Health Insurance: ";
+    fout << setw(18) << right << setfill('.') << health << endl;
+    fout << setw(18) << left << setfill('.') <<  "Net Pay: ";
+    fout << setw(18) << right << setfill('.') << net << endl;
     
     /*cout.width(45);
     cout << setfill('*') << setw(45);
